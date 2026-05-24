b244bc804817debcfd52f17b4d3b1cfa8284a23b
Arbind Das <dasarbind269@gmail.com>
2026-05-24 12:30:28 +0545

    Add frontend project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
-import "./index.css";
-import Navbar from "./components/Navbar";
-import Login from "./pages/auth/Login";
-import Signup from "./pages/auth/Signup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import PrincipalDashboard from "./pages/dashboard/Principal/PrincipalDashboard"
-import TeacherDashboard from "./pages/dashboard/Teacher/TeacherDashboard"
-import StudentDashboard from "./pages/dashboard/Student/StudentDashboard"
-import LibraryDashboard from "./pages/dashboard/Librarian/LibrarianDashboard"
-import ProtectedRoute from "./routes/ProtectedRoute"
-import Unauthorized from "./pages/errors/Unauthorized";
-import NotFound from "./pages/errors/NotFound";
- export default function App() {
-     return (
-       <BrowserRouter>
-         <Navbar isAuthenticated={false} />
-         <Routes>
 
-            <Route path="/unauthorized" element={<Unauthorized />} />
-           <Route path="/" element={<Home/>} />
-           <Route path="/signup" element={<Signup />} />
-           <Route path="/login"  element={<Login />}  />
-          
-           {/* Dashboards */}
+import './App.css'
 
+function App() {
+ 
 
-           <Route
-            path="/principal"
-            element={
-              <ProtectedRoute allowedRoles={["principal"]}>
-                <PrincipalDashboard/>
-              </ProtectedRoute>
-            }
-           />
+  return (
+   <div>
+    jai shree ram
+   </div>
+  )
+}
 
-           <Route
-           path="/teacher"
-           element={
-            <ProtectedRoute allowedRoles={["teacher"]}>
-                <TeacherDashboard/>
-            </ProtectedRoute>
-           }
-           />
-
-
-
-           <Route
-           path="/student"
-           element={
-            <ProtectedRoute allowedRoles={["student"]}>
-              <StudentDashboard/>
-            </ProtectedRoute>
-           }
-           />
-
-
-           <Route
-
-            path="/librarian"
-            element={
-              <ProtectedRoute allowedRoles={['librarian']}>
-                  <LibraryDashboard/>
-              </ProtectedRoute>
-            }
-           />
-
-            <Route path="*" element={<NotFound />} /> 
-         </Routes>
-       </BrowserRouter>
-     );
-   }
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import { authService } from "./services/authService";
-authService.init(); // ← restores Authorization header on reload
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
